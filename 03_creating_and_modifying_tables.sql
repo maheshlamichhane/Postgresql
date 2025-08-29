9331e4eae22725ef6ab029896b197f9864fcb41e
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-08-30 00:00:53 +0545

    added first commit

---
+++ 03_creating_and_modifying_tables.sql
@@ -0,0 +1,61 @@
+-- Create the actor table
+-- function: CREATE Table
+
+CREATE TABLE actors(
+	actor_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(150),
+	last_name VARCHAR(150) NOT NULL,
+	gender CHAR(1),
+	date_of_birth DATE,
+	add_date DATE,
+	update_date DATE
+);
+
+-- Create the directors table
+CREATE TABLE directors(
+	director_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(150),
+	last_name VARCHAR(150),
+	date_of_birth DATE,
+	nationality VARCHAR(20),
+	add_date DATE,
+	update_date DATE
+);
+
+-- Create the movies table
+-- Movies > Directors
+CREATE TABLE movies(
+	movie_id SERIAL PRIMARY KEY,
+	movie_name VARCHAR(100) NOT NULL,
+	movie_length INT,
+	movie_lang VARCHAR(20),
+	age_certificate VARCHAR(10),
+	release_date DATE,
+	director_id INT REFERENCES directors (director_id)
+);
+
+-- Creating a movies_revenues table 
+CREATE TABLE movies_revenues(
+	revenue_id SERIAL PRIMARY KEY,
+	movie_id INT REFERENCES movies (movie_id),
+	revenues_domestic NUMERIC(10,2),
+	revenues_international NUMERIC(10,2)
+);
+
+-- Creating a movies_actors junction table
+CREATE TABLE movies_actors(
+	movie_id INT REFERENCES movies (movie_id),
+	actor_id INT REFERENCES actors (actor_id),
+	PRIMARY KEY (movie_id,actor_id)
+);
+select * from movies_actors;
+
+
+
+
+
+
+
+
+
+
---
+++ 04_modifying_data_in_tables.sql
@@ -0,0 +1,79 @@
+-- CREATE sample table
+CREATE TABLE customers(
+	customer_id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(150),
+	age INT
+);
+
+
+-- Insert single data
+INSERT INTO customers (first_name,last_name,email,age)
+values ('Adnan','waheed','a@b.com',40);
+
+-- Insert multiple data
+INSERT INTO customers(first_name,last_name)
+values
+('ADNAN','WAHEED'),
+('JOHN','ADAMS'),
+('LINDA','ABE');
+
+-- Insert data containng quote
+INSERT INTO customers (first_name)
+VALUES ('Bill''O Sullivan');
+
+-- First see the default behaviour when adding a record into9 a table
+INSERT INTO customers (first_name)
+VALUES ('ADAM');
+
+-- After the insert, lets returns all rows
+INSERT INTO customers (first_name)
+VALUES ('JOSEPH') RETURNING *;
+
+-- Update single column
+UPDATE customers SET email='a2@b.com' where customer_id=1;
+
+-- Update multiple columns
+UPDATE customers SET email='a4@b.com',age=30 where customer_id=1;
+
+-- Use RETURNING to get updated rows
+UPDATE customers SET email='a@b.com' where customer_id=3 RETURNING *;
+
+-- Update all records in a table
+UPDATE customers SET is_enabled='Y' WHERE customer_id = 1;
+UPDATE customers SET is_enabled = 'Y';
+
+
+-- To delete records based on a condition
+DELETE FROM customers WHERE customer_id=9;
+
+-- To delete without condition
+DELETE FROM customers;
+
+-- UPSERT
+-- Create sample table 
+
+CREATE TABLE t_tags(
+	id SERIAL PRIMARY KEY,
+	tag TEXT UNIQUE,
+	update_date TIMESTAMP DEFAULT NOW()
+);
+
+-- Insert some sample data
+INSERT INTO t_tags (tag) VALUES ('Pen'),('Pencil');
+
+-- Lets insert a record, o9n conflict do nothing
+INSERT INTO t_tags (tag) VALUES ('Pen') ON CONFLICT (tag) DO NOTHING;
+INSERT INTO t_tags (tag) VALUES ('Pen')
+ON CONFLICT (tag) DO UPDATE SET tag = tag;
+
+SELECT * from t_tags;
+
+
+
+
+
+
+
+
